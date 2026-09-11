6b83f74d783ae05da458e1f40ad2b32034126740
Mike Splain <mike.splain@gmail.com>
2026-09-11 09:53:28 -0400

    Update Unsloth to 0.1.808-beta
    - Bump version from 0.1.70-beta to 0.1.808-beta
    - Fix URL template to match new asset naming (version removed from
      filename starting at v0.1.804-beta)
    - Update sha256 checksum

--- Casks/unsloth.rb
+++ Casks/unsloth.rb
@@ -1,8 +1,8 @@
 cask "unsloth" do
-  version "0.1.70-beta"
-  sha256 "6f49784d431dbb5679892f9e1964fe2fde2f1f1feffa86d8b7991a59219dd3b6"
+  version "0.1.808-beta"
+  sha256 "e868ea223de0268959de834d0765f92d20a30215c4c697f7e4a208e3265befd6"
 
-  url "https://github.com/unslothai/unsloth/releases/download/v#{version}/Unsloth-Desktop-#{version.tr(".-", "__")}-MacOS.dmg"
+  url "https://github.com/unslothai/unsloth/releases/download/v#{version}/Unsloth-Desktop-MacOS.dmg"
   name "Unsloth"
   desc "Desktop app for running and training open models locally"
   homepage "https://github.com/unslothai/unsloth"
